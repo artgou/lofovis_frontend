b21e98856a851961fcef9fcb353e2d7b62f3f28e
artgou <artgou@163.com>
2021-03-13 12:50:36 +0800

    .

---
+++ src/official/manage/list.js
@@ -0,0 +1,12 @@
+require('../common/common');
+require('./list.less');
+
+$(function () {
+  //提交
+  function submit() {
+    const fileList = getFileList();
+    console.log({ fileList });
+  }
+
+  $('#btnSubmit').on('click', submit);
+});
