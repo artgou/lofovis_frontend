8333f06bda3093dc132cd35b083432c03b552a65
artgou <artgou@163.com>
2021-03-16 22:32:07 +0800

    .

--- src/official/article/detail_image.js
+++ src/official/article/detail_image.js
@@ -1,11 +1,21 @@
 require('./detail.less');
 
 $(function () {
-  var swiper = new Swiper('.swiper-container', {
+  var outlineSwiper = new Swiper('.swiper-outline', {
     direction: 'vertical',
     noSwipingClass: 'swiper-scroll',
     navigation: {
       prevEl: '.btn-swiper-pre',
+      nextEl: '.panel-center',
+    },
+  });
+
+  var inlineSwiper = new Swiper('.swiper-inline', {
+    direction: 'vertical',
+    noSwipingClass: 'swiper-scroll',
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
     },
   });
 });
